926c6f44b92a94c17e72ac966d8054bb94f5b1b5
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-05 23:07:35 -0300

    Feat:Creating .NET series registration application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitalInnovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
